84988b20efee3a51adcbfa9e514021b484120d1d
Anton <yud@dff.com>
2022-08-23 21:33:04 +0300

    version 2

--- Program.cs
+++ Program.cs
@@ -1,54 +1,49 @@
 ﻿Random rd = new Random();
+int input;
 
-Console.WriteLine("DZ zadacha 2:");
-int input1 = 0;
-int input2 = 0;
-System.Console.WriteLine(input1 = rd.Next(1, 100));
-System.Console.WriteLine(input2 = rd.Next(1, 100));
-if (input1 > input2)
+//Zadacha 10:
+Console.WriteLine("Zadacha 10");
+Console.WriteLine(input = rd.Next(100, 1000));
+input = (input / 10) % 10;
+Console.WriteLine(input);
+
+//Zadacha 13:
+Console.WriteLine("Zadacha 13");
+Console.WriteLine(input = rd.Next(1, 200));
+if (input > 99)
 {
-    System.Console.WriteLine($"max = {input1}");
-
+    Console.WriteLine(input = (input / 100) % 10);
 }
 else
 {
-    System.Console.WriteLine($"max = {input2}");
+    Console.WriteLine("третьей цифры нет");
 }
 
-Console.WriteLine("DZ zadacha 4:");
-int input3 = 0;
-System.Console.WriteLine(input1 = rd.Next(1, 100));
-System.Console.WriteLine(input2 = rd.Next(1, 100));
-System.Console.WriteLine(input3 = rd.Next(1, 100));
-if (input1 > input2 & input1 > input3)
-{
-    System.Console.WriteLine($"max = {input1}");
-}
-if (input2 > input1 & input2 > input3)
-{
-    System.Console.WriteLine($"max = {input2}");
-}
-if (input3 > input1 & input3 > input2)
-{
-    System.Console.WriteLine($"max = {input3}");
-}
 
-Console.WriteLine("DZ zadacha 6:");
-int input = 0;
-System.Console.WriteLine(input = rd.Next(-10, 11));
-if (input % 2 == 0)
+//Zadacha 15
+Console.WriteLine("Zadacha 15");
+Console.WriteLine(input = rd.Next(-10, 11));
+if (input > 5 || input < 8)
 {
-    Console.WriteLine("Число чётное!");
+    Console.WriteLine("нет");
 }
 else
 {
-    Console.WriteLine("Число не чётное!");
+    if (input == 2 || input == 7) { Console.WriteLine("да"); }
 }
 
-Console.WriteLine("DZ zadacha 8:");
-input = rd.Next(1,100);
-int count = (input / 2);
-for (int index = 1; index < count + 1; index++)
+//задача необязательная.
+Console.WriteLine("задача необязательная.");
+Console.WriteLine("Введите кол-во программистов: ");
+int programmer_count = Convert.ToInt32(Console.ReadLine());
+string end = "";
+if (programmer_count % 10 > 1 & programmer_count % 10 < 5)
 {
-    Console.Write($"{index * 2}, ");
+    end = "а";
 }
+if (programmer_count % 10 > 4 || programmer_count % 10 < 1
+    || programmer_count % 100 > 10 & programmer_count % 100 < 21)
+{
+    end = "ов";
+}
+Console.Write($"{programmer_count} программист{end}");
