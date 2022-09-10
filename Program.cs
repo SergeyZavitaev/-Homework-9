0bbf244eb7b66ba396d11f99c842a9c74a541ac2
Anton <yud@dff.com>
2022-09-10 16:33:34 +0300

    dz 6

--- Program.cs
+++ Program.cs
@@ -1,106 +1,81 @@
 ﻿Random rd = new Random();
 
-//Задача 34: Задайте массив заполненный
-//случайными положительными трёхзначными
-//числами. Напишите программу, которая
-//покажет количество чётных чисел в массиве.
-//[345, 897, 568, 234] -> 2
-void countEvenNumbers(int count)
+//Задача 41: Пользователь вводит
+//с клавиатуры M чисел. Посчитайте
+//, сколько чисел больше 0
+//ввёл пользователь.
+//5
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+void Mchisel()
 {
-    int[] massive = new int[count];
+    List<int> ListOfElements = new List<int>();
+    int input = 0;
+
+    bool stopGive = false; //переменная, которая сообщает нужно ли ещё сортировать или готово
+    while (stopGive == false) //цикл пока не отсортирует
+    {
+        try
+        {
+            Console.Write("");
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input > 0)
+            {
+                ListOfElements.Add(input);
+            }
+        }
+        catch
+        {
+            string inputStr = Console.ReadLine();
+            if (inputStr == "")
+            {
+                stopGive = true;
+            }
+        }
+    }
+
+    int[] massive = ListOfElements.ToArray();
     for (int i = 0; i < massive.Length; i++)
     {
          massive[i] = rd.Next(100, 1000);
          Console.Write($"{massive[i]}, ");
     }
-    //Console.WriteLine("");
-    int otvet = 0;
-    for (int i = 0; i < count; i++)
-    {
-        if (massive[i] % 2==0)
-        {
-            otvet++;
-        }
-    }
-    Console.WriteLine($"-> {otvet}");
+    Console.WriteLine($"-> {ListOfElements.Count}");
 }
 
-//Задача 36: Задайте одномерный массив,
-//заполненный случайными числами.
-//Найдите сумму элементов, стоящих
-//на нечётных позициях.
-//[3, 7, 23, 12] -> 19
-//[-4, -6, 89, 6]-> 0
+//Задача 43: Напишите программу,
+//которая найдёт точку пересечения
+//двух прямых, заданных уравнениями
+//y = k1 * x + b1, y = k2 * x + b2;
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5)
 
-void summaNotEvenNumbers(int count)
+void TwoLinesCrossing()
 {
-    int[] massive = new int[count];
-    for (int i = 0; i < massive.Length; i++)
+    double[] coords = new double[6];
+    double giveOtvetDoub(string message)
     {
-        massive[i] = rd.Next(-50, 100);
-        Console.Write($"{massive[i]}, ");
+        Console.Write(message);
+        return Convert.ToDouble(Console.ReadLine());
     }
-    
-    int summa = 0;
-    for (int i = -1; i < count; i++)
-    {
-        if (i % 2 == 0)
-        {
-            summa=summa+massive[i+1];
-        }
-    }
-    Console.WriteLine($"-> {summa}");
+    coords[0] = giveOtvetDoub("Введите координату b1: "); //b1
+    coords[1] = giveOtvetDoub("Введите координату k1: "); //k1
+    coords[2] = giveOtvetDoub("Введите координату b2: "); //b2
+    coords[3] = giveOtvetDoub("Введите координату k2: "); //k2
+
+    coords[4] = -1 * ((coords[0] - coords[2]) / (coords[1] - coords[3]));
+    coords[5] = -1 * (coords[1]*( -1 * coords[4]) - coords[0]);
+
+    Console.Write(coords[4].ToString());
+    Console.Write(coords[5].ToString());
+    int input = 0;
 }
-
-//Задача 38: Задайте массив вещественных чисел.
-//Найдите разницу между максимальным и
-//минимальным элементов массива.
-//[3 7 22 2 78] -> 76
-
-void differenceMaxAndMin(int count)
-{
-    double[] massive = new double[count];
-    for (int i = 0; i < massive.Length; i++)
-    {
-        massive[i] = rd.NextDouble()*1000;
-        Console.Write($"{massive[i]} ");
-    }
-
-    //max
-    double max = massive[0];
-    int maxIndex = 0;
-    for (int i = 0; i < massive.Length; i++)
-    {
-        if (massive[i] > max)
-        {
-            max = massive[i];
-            maxIndex = i;
-        }
-    }
-
-    //min
-    double min = massive[0];
-    int minIndex = 0;
-    for (int i = 0; i < massive.Length; i++)
-    {
-        if (massive[i] < max)
-        {
-            min = massive[i];
-            minIndex = i;
-        }
-    }
-
-    Console.WriteLine($"-> {max-min}");
-}
-
 try
 {
-    Console.WriteLine("Задача 34: ");
-    countEvenNumbers(4);
-    Console.WriteLine("Задача 36: ");
-    summaNotEvenNumbers(4);
-    Console.WriteLine("Задача 38: ");
-    differenceMaxAndMin(5);
+    Console.WriteLine("Задача 41: ");
+    Mchisel();
+    Console.WriteLine("Задача 43: ");
+    TwoLinesCrossing();
 }
 catch
 {
